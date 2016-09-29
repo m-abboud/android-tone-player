dacc39d4613dd60c3ef1fb9ffb4a5fd30dfd1eac
meese <matt.w.abboud@gmail.com>
2016-09-29 02:21:36 -0600

    update gradle build for bintray/jcenter publishing and add an additional app test

--- src/androidTest/java/net/mabboud/android_tone_player/TonePlayerTest.java
+++ src/androidTest/java/net/mabboud/android_tone_player/TonePlayerTest.java
@@ -2,6 +2,7 @@ package net.mabboud.android_tone_player;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import org.junit.Assert;
 import org.testng.annotations.Test;
 
 public class TonePlayerTest extends ApplicationTestCase<Application> {
@@ -15,5 +16,16 @@ public class TonePlayerTest extends ApplicationTestCase<Application> {
         OneTimeBuzzer buzzer = new OneTimeBuzzer(3);
         buzzer.play();
         buzzer.stop();
+    }
+
+    @Test
+    public void givenOneTimePlayer_whenPlayed_thenIsStoppedAfterWaiting() throws InterruptedException {
+        // bit of a smoke test to make sure nothing goes terribly wrong
+        OneTimeBuzzer buzzer = new OneTimeBuzzer(.2);
+
+        buzzer.play();
+        Thread.sleep(1000);
+
+        Assert.assertFalse(buzzer.isPlaying);
     }
 }
